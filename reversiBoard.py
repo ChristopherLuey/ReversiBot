e0d78971783785481d1b6aa73853756ed32107cf
Kyler <kylerrosen@gmail.com>
2020-04-21 13:49:13 -0700

    updated some colors

--- reversiBoard.py
+++ reversiBoard.py
@@ -32,26 +32,29 @@ class Board:
 	def startGame(self):
 		win = GraphWin("Decide Player", 500, 200)
 		mover, moveg, moveb = 90, 117, 8
+
 		for i in range(104, 0, -1):
 			c = Circle(Point(650, 375), i * 10)
 			c.setFill(color_rgb(int(9 + mover * i / 102), int(130 - moveg * i / 102), int(230 - moveb * i / 102)))
 			c.setOutline(color_rgb(int(9 + mover * i / 102), int(130 - moveg * i / 102), int(230 - moveb * i / 102)))
 			c.setWidth(10)
 			c.draw(win)
+
 		t = Text(Point(250, 40), "Would you like to play Black or White?").draw(win)
 		t.setSize(25)
 		t.setStyle("bold")
 
 		b = Button(Point(150, 140), 50, 125, "Black")
 		b.draw(win)
-		b.setColor("black", "darkgoldenrod")
+		b.setColor("dimgrey", "black")
 		w = Button(Point(350, 140), 50, 125, "White")
 		w.draw(win)
-		w.setColor("white", "darkgoldenrod")
+		w.setColor("white", "lightgrey")
 		w.setTextColor("black")
 		b.setActive()
 		w.setActive()
 		p = win.getMouse()
+		
 		while True:
 			if b.isClicked(p):
 				return 'black'
