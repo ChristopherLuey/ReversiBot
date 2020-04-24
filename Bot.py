89a29ea29aa42b40dfeb859adcb437ce0cddee0d
Kyler <kylerrosen@gmail.com>
2020-04-24 12:34:19 -0700

    Bot Fix

--- Bot.py
+++ Bot.py
@@ -7,3 +7,23 @@ class Bot:
     def __init__(self, player, boardState, turn):
         self.player = player
         self.board = Board(boardState, turn, player)
+
+	def alphabeta(node, depth, α, β, maximizingPlayer) is
+	    if depth = 0 or node is a terminal node then
+	        return the heuristic value of node
+	    if maximizingPlayer then
+	        value := −∞
+	        for each child of node do
+	            value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
+	            α := max(α, value)
+	            if α ≥ β then
+	                break (* β cut-off *)
+	        return value
+	    else
+	        value := +∞
+	        for each child of node do
+	            value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
+	            β := min(β, value)
+	            if α ≥ β then
+	                break (* α cut-off *)
+	        return value        
