fdc2e049a1d194b93fb652ab7c456deee9ab4236
Chrisotpher Luey <cluey21@csus.org>
2020-04-27 21:08:43 -0700

    mknjbhv

---
+++ lmao.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import *
+
+
+Stock_Market = {'Year': [2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2017,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016,2016],
+                'Month': [12, 11,10,9,8,7,6,5,4,3,2,1,12,11,10,9,8,7,6,5,4,3,2,1],
+                'Interest_Rate': [2.75,2.5,2.5,2.5,2.5,2.5,2.5,2.25,2.25,2.25,2,2,2,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75,1.75],
+                'Unemployment_Rate': [5.3,5.3,5.3,5.3,5.4,5.6,5.5,5.5,5.5,5.6,5.7,5.9,6,5.9,5.8,6.1,6.2,6.1,6.1,6.1,5.9,6.2,6.2,6.1],
+                'Stock_Index_Price': [1464,1394,1357,1293,1256,1254,1234,1195,1159,1167,1130,1075,1047,965,943,958,971,949,884,866,876,822,704,719]
+                }
+
+df = pd.DataFrame(Stock_Market,columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price'])
+
+X = df[['Interest_Rate','Unemployment_Rate']]
+Y = df['Stock_Index_Price']
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+print(X)
